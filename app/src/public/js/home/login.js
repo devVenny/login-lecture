3577ce175593fbfb94ef67966ef827dca89431ae
vennylee <hyungsup42134@gmail.com>
2022-01-19 18:47:25 +0900

    login화면  js로 제어와 POST

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("hi");
+const id = document.getElementById("id"),
+  psword = document.getElementById("psword"),
+  loginBtn = document.getElementById("loginBtn");
+
+console.log(id, psword, loginBtn);
+
+loginBtn.addEventListener("click", () => {
+  const req = {
+    id: id.value,
+    psw: psword.value,
+  };
+
+  console.log(req);
+  console.log(JSON.stringify(req));
+  fetch("login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+});
