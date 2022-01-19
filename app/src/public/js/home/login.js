9ffe89bb62201fe417572c30f725203be5a86fb0
vennylee <hyungsup42134@gmail.com>
2022-01-19 22:52:43 +0900

    route controler 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,16 +4,12 @@ const id = document.getElementById("id"),
   psword = document.getElementById("psword"),
   loginBtn = document.getElementById("loginBtn");
 
-console.log(id, psword, loginBtn);
-
 loginBtn.addEventListener("click", () => {
   const req = {
     id: id.value,
     psw: psword.value,
   };
 
-  console.log(req);
-  console.log(JSON.stringify(req));
   fetch("login", {
     method: "POST",
     headers: {
